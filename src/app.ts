66327fd1807750e47f58311f931c91c80ded1135
RafaelRufino <rafaelrufino.52@gmail.com>
2021-10-20 03:45:46 -0300

    finalizando o backend

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import "dotenv/config";
 import express = require("express");
+import { router } from "./routes";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+app.use(router);
 
 app.get("/github", (req, res) => {
   res.redirect(
@@ -9,4 +27,10 @@ app.get("/github", (req, res) => {
   );
 });
 
-app.listen(3333, () => console.log("🚀 Server is runniing on port 3333"));
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+
+  return res.json(code);
+});
+
+export { serverHttp, io };
